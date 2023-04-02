d76bfd9fa2680c6e4084c1a8b701f587024a1efb
coderyjc <coderyjc@163.com>
2023-04-02 19:36:29 +0800

    add md5util

---
+++ certificate_analysis/backend/certificate/src/test/java/top/coderyjc/certificate/UserTest/UserMapperTest.java
@@ -0,0 +1,9 @@
+package top.coderyjc.certificate.UserTest;/**
+* ClassName: UserInsertTest
+* Package: top.coderyjc.certificate.UserTest
+* Description: 
+* @Author Yan Jingcun
+* @Create 4/2/2023 7:27 PM 
+* @Version 1.0
+*/public class UserInsertTest {
+}
