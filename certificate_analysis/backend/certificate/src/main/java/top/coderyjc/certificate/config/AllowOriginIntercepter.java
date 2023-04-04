2315afc4aea4023aea262677af4610816bf495be
coderyjc <coderyjc@163.com>
2023-04-04 20:49:54 +0800

    用户登录功能完成

---
+++ certificate_analysis/backend/certificate/src/main/java/top/coderyjc/certificate/config/AllowOriginIntercepter.java
@@ -0,0 +1,9 @@
+package top.coderyjc.certificate.config;/**
+* ClassName: AllowOriginIntercepter
+* Package: top.coderyjc.certificate.config
+* Description: 
+* @Author Yan Jingcun
+* @Create 4/4/2023 8:19 PM 
+* @Version 1.0
+*/public class AllowOriginIntercepter {
+}
---
+++ certificate_analysis/backend/certificate/src/main/java/top/coderyjc/certificate/config/MvcConfig.java
@@ -0,0 +1,13 @@
+package top.coderyjc.certificate.config;
+
+/**
+ * ClassName: MvcConfig
+ * Package: top.coderyjc.certificate.config
+ * Description:
+ *
+ * @Author Yan Jingcun
+ * @Create 4/4/2023 8:20 PM
+ * @Version 1.0
+ */
+public class MvcConfig {
+}
