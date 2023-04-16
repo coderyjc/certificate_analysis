2c8a1e9d8e1c8e892f07faa01d8a0a1f1775732f
coderyjc <coderyjc@163.com>
2023-04-16 10:16:44 +0800

    Dashboard功能完成

---
+++ certificate_analysis/backend/certificate/src/main/java/top/coderyjc/certificate/controller/DashboardController.java
@@ -0,0 +1,9 @@
+package top.coderyjc.certificate.controller;/**
+* ClassName: DashboardController
+* Package: top.coderyjc.certificate.controller
+* Description: 
+* @Author Yan Jingcun
+* @Create 4/15/2023 8:22 PM 
+* @Version 1.0
+*/public class DashboardController {
+}
