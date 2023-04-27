a4ea96d2fdf1b4592112c07e0b75242dc6d19663
coderyjc <coderyjc@163.com>
2023-04-27 08:39:38 +0800

    小修小补

---
+++ certificate_analysis/backend/certificate/src/main/java/top/coderyjc/certificate/controller/WrittenVisualizeController.java
@@ -0,0 +1,9 @@
+package top.coderyjc.certificate.controller;/**
+* ClassName: WrittenVisualizeController
+* Package: top.coderyjc.certificate.controller
+* Description: 
+* @Author Yan Jingcun
+* @Create 4/27/2023 8:35 AM 
+* @Version 1.0
+*/public class WrittenVisualizeController {
+}
